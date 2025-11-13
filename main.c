80069fad9f6afc74883ff335d2fdaa6e5161d62b
yerimlee <144992260+limye2@users.noreply.github.com>
2025-11-13 15:32:43 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -7,12 +7,19 @@
 
 #include <stdio.h>
 
+void swap(int* x, int* y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main(int argc, const char * argv[]) {
-    int i = 300;
+    int a = 3;
+    int b = 5;
     
-    int *pi = &i;
-    char *pc = &i;
-    
-    printf("%i, %i, %i\n", i, *pi, *pc); // 300, 300, 44
+    swap(&a,&b);
+    printf("a:%i,b:%i\n", a, b);
     return 0;
 }
