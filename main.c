93479e453f124eb39fb2b5d175ded4b5e50d7099
yerimlee <144992260+limye2@users.noreply.github.com>
2025-11-13 15:22:43 +0900

    실습2

--- main.c
+++ main.c
@@ -8,18 +8,11 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int i = 10;
-    char c = 'a';
-    int *iptr;
-    char *cptr;
-    int *iptr2;
+    int i = 300;
     
-    iptr = &i;
-    cptr = &c;
-    iptr2 = iptr;
+    int *pi = &i;
+    char *pc = &i;
     
-    printf("i: %p\n%p(size:%i)\n", iptr, &i, sizeof(iptr));
-    printf("c: %p\n%p(size:%i)\n", cptr, &c, sizeof(cptr));
-    printf("iptr2: %p, %i\n", iptr2, *iptr2);
+    printf("%i, %i, %i\n", i, *pi, *pc); // 300, 300, 44
     return 0;
 }
